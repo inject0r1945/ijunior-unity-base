ee2bf0d4e3b800dd09bdd8b3a52ac657612666b2
inject0r <di-vasilev90@ya.ru>
2023-08-14 13:00:22 +0700

    Новое домашнее задание

---
+++ homework11_code_style_genius/PointsPatrol.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsPatrol : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private Transform _pointsParent;
+
+    private Transform[] _patrolPoints;
+    private int _patrolPointIndex;
+    private Transform _destinationPatrolPoint;
+
+    private void Start()
+    {
+        InitializePatrolPointsArray();
+    }
+
+    private void Update()
+    {
+        _destinationPatrolPoint = _patrolPoints[_patrolPointIndex];
+
+        transform.position = Vector3.MoveTowards(transform.position, _destinationPatrolPoint.position, _moveSpeed * Time.deltaTime);
+
+        if (transform.position == _destinationPatrolPoint.position)
+        {
+            CalculateNextPatrolPointIndex();
+            RotateToDestination();
+        }
+    }
+
+    private void InitializePatrolPointsArray()
+    {
+        _patrolPoints = new Transform[_pointsParent.childCount];
+
+        for (int i = 0; i < _pointsParent.childCount; i++)
+            _patrolPoints[i] = _pointsParent.GetChild(i).GetComponent<Transform>();
+    }
+
+    public void CalculateNextPatrolPointIndex()
+    {
+        _patrolPointIndex++;
+
+        if (_patrolPointIndex == _patrolPoints.Length)
+            _patrolPointIndex = 0;
+    }
+
+    private void RotateToDestination()
+    {
+        Vector3 currentDestination = _patrolPoints[_patrolPointIndex].transform.position;
+        transform.forward = currentDestination - transform.position;
+    }
+}
---
+++ homework11_code_style_genius/Shooter.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _bulletSpeed;
+    [SerializeField] private float _pauseBetweenShots;
+    [SerializeField] private GameObject _bulletPrefab;
+
+    void Start() 
+    {
+        StartCoroutine(nameof(Shoot));
+    }
+
+    private IEnumerator Shoot()
+    {
+        bool isEnabledComponent = enabled;
+        var waitForSeconds = new WaitForSeconds(_pauseBetweenShots);
+
+        while (isEnabledComponent)
+        {
+            Vector3 shootingDirection = (_target.position - transform.position).normalized;
+            GameObject bullet = Instantiate(_bulletPrefab, transform.position + shootingDirection, Quaternion.identity);
+
+            Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+
+            bulletRigidbody.transform.up = shootingDirection;
+            bulletRigidbody.velocity = shootingDirection * _bulletSpeed;
+
+            yield return waitForSeconds;
+         }
+    }
+}
